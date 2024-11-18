963eaeddf9d25e759c5a7fd50e247f4c2288dbd1
Angel Osalde <80134042+Ninthharp8@users.noreply.github.com>
2024-11-17 20:43:26 -0600

    se añadió la funcionalidad para detectar comentarios en bloque y omitirlos
    Se añadió la funcionalidad mediante condicionales que detectan los comentarios en bloque al inicio del archivo u en cualquier parte del mismo y bloques de una sola linea. Luego de detectarlos los omite para que no sean contados como líneas físicas o lógicas por error.

--- Analizador_De_Codigo.py
+++ Analizador_De_Codigo.py
@@ -20,9 +20,32 @@ class AnalizadorDeCodigo:
         ]
 
     def analizar_archivo(self):
+        comentario_bloque = False
+
         with open(self.ruta_del_archivo, 'r', encoding='utf-8') as archivo:
             for linea in archivo:
                 linea_sin_espacios = linea.strip()
+
+                """
+                Manejo de comentarios en bloque ignora comentarios en bloque si estos estan en cualquier 
+                parte del archivo o si estan en una misma linea.
+                """
+
+                if '"""' in linea_sin_espacios or "'''" in linea_sin_espacios:
+                    # Si el bloque de comentarios empieza y termina en la misma línea, lo ignoramos.
+                    if linea_sin_espacios.count('"""') == 2 or linea_sin_espacios.count("'''") == 2:
+                        continue
+                    # Si ya estamos dentro de un bloque de comentarios, marcamos que terminó el bloque.
+                    if comentario_bloque:
+                        comentario_bloque = False
+                        continue
+                    # Si encontramos el inicio de un bloque de comentarios, marcamos que estamos dentro del bloque.
+                    comentario_bloque = True
+                    continue
+
+                # decidimos si ignorar o no lo procecesado. si es True se salta la linea y no se cuenta.
+                if comentario_bloque:
+                    continue
 
                 # Ignorar líneas en blanco
                 if not linea_sin_espacios:
@@ -36,7 +59,7 @@ class AnalizadorDeCodigo:
                 self.lineas_fisicas += 1
 
                 # Contar líneas lógicas solo si están definidas en las palabras clave
-                primera_palabra = linea_sin_espacios.split()[0]  
+                primera_palabra = linea_sin_espacios.split()[0]
                 if primera_palabra.rstrip(':') in self.palabras_clave_logicas:
                     self.lineas_logicas += 1
 
@@ -54,7 +77,7 @@ class AnalizadorDeCodigo:
 
 if __name__ == "__main__":
     # Para pruebas con un solo archivo. Si son varios, usa AutoTest.py
-    ruta_del_archivo = './pruebas/Prueba_G.py'  
+    ruta_del_archivo = './pruebas/Prueba_A.py'
     analizador = AnalizadorDeCodigo(ruta_del_archivo)
     analizador.analizar_archivo()
     analizador.informe()
